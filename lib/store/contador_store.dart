e993b0295475441210cadabd8568983b7890b552
Alexandre Freitas <xande_liborio@hotmail.com>
2022-05-27 14:54:00 -0300

    458. MobX na Prática

---
+++ lib/store/contador_store.dart
@@ -0,0 +1,20 @@
+import 'package:mobx/mobx.dart';
+
+part 'contador_store.g.dart';
+
+class ContadorStore = _ContadorStore with _$ContadorStore;
+
+abstract class _ContadorStore with Store {
+  @observable
+  int contador = 0;
+
+  @action
+  void incrementar() {
+    contador++;
+  }
+
+  @action
+  void decrementar() {
+    contador--;
+  }
+}
